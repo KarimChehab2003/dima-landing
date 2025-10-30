5bb9872cf682f646658dc6583d0eee406f0ca46f
KarimChehab2003 <karimchehab2018@gmail.com>
2025-10-30 20:11:04 +0300

    - Adjusted QuestionsAnswered Section

--- app/[locale]/(home)/components/QuestionsAccordion.tsx
+++ app/[locale]/(home)/components/QuestionsAccordion.tsx
@@ -1,11 +1,21 @@
 "use client";
 
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
-import { questionsAnsweredInfo } from "@/data/constants/info";
 import { useState } from "react";
+import { useLocale, useTranslations } from "next-intl";
 
 function QuestionsAccordion() {
     const [openItem, setOpenItem] = useState<string>("item-1");
+    const t = useTranslations("Home.questionsAnswered.faqs");
+    const locale = useLocale();
+    const isRTL = locale === "ar";
+
+    const faqs = [
+        { key: "faq1", question: t("faq1.question"), answer: t("faq1.answer") },
+        { key: "faq2", question: t("faq2.question"), answer: t("faq2.answer") },
+        { key: "faq3", question: t("faq3.question"), answer: t("faq3.answer") }
+    ];
+
     return (
         <Accordion
             type="single"
@@ -16,17 +26,21 @@ function QuestionsAccordion() {
             className="w-full bg-muted rounded-xl p-2 space-y-4"
         >
             {
-                questionsAnsweredInfo.map(({ question, answer }, i) => {
+                faqs.map(({ key, question, answer }, i) => {
                     const value = `item-${i + 1}`;
                     const isOpen = value === openItem;
 
                     return <AccordionItem
-                        key={question}
+                        key={key}
                         value={value}
                         className="bg-white rounded-xl py-3 px-6"
                     >
-                        <AccordionTrigger className={`text-lg transition-all hover:no-underline ${isOpen ? "font-bold" : "font-medium"}`}>{question}</AccordionTrigger>
-                        <AccordionContent className="text-sm/relaxed">{answer}</AccordionContent>
+                        <AccordionTrigger className={`text-lg transition-all hover:no-underline ${isOpen ? "font-bold" : "font-medium"} ${isRTL ? "flex-row-reverse" : "flex-row"}`}>
+                            {question}
+                        </AccordionTrigger>
+                        <AccordionContent className="text-sm/relaxed">
+                            {answer}
+                        </AccordionContent>
                     </AccordionItem>
                 })
             }
--- app/[locale]/(home)/sections/QuestionsAnsweredSection.tsx
+++ app/[locale]/(home)/sections/QuestionsAnsweredSection.tsx
@@ -4,17 +4,20 @@ import { Button } from "@/components/ui/button";
 import SectionWrapper from "../components/SectionWrapper";
 import { ArrowRight } from "lucide-react";
 import QuestionsAccordion from "../components/QuestionsAccordion";
-
+import { useLocale, useTranslations } from "next-intl";
 
 function QuestionsAnsweredSection() {
+    const t = useTranslations("Home.questionsAnswered");
+    const locale = useLocale();
+    const isRTL = locale === "ar";
     return (
         <SectionWrapper>
-            <div className="container mx-auto max-w-6xl flex flex-col lg:flex-row justify-between gap-12">
+            <div className={`container mx-auto max-w-6xl flex flex-col lg:flex-row justify-between gap-12 ${isRTL ? 'lg:flex-row-reverse text-right' : ' text-left'}`}>
                 <div className="space-y-6">
-                    <h2 className="text-3xl sm:text-4xl font-bold text-primary capitalize">Your questions, answered</h2>
-                    <p className="text-muted-foreground font-medium">Get quick answers to the most common questions about our platform and services.</p>
+                    <h2 className="text-3xl sm:text-4xl font-bold text-primary capitalize">{t("title")}</h2>
+                    <p className="text-muted-foreground font-medium">{t("description")}</p>
                     <Button size="lg">
-                        Request a demo
+                        {t("request a demo")}
                         <div className="w-6 h-6 rounded-full bg-white flex justify-center items-center">
                             <ArrowRight color="black" />
                         </div>
