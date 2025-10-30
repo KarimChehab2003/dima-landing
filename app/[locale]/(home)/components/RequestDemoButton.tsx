ac8a8e6c96c7c824a5f21a53c112b374473804c5
KarimChehab2003 <karimchehab2018@gmail.com>
2025-10-30 17:18:01 +0300

    - Adjusted Dima ai section

--- app/[locale]/(home)/components/RequestDemoButton.tsx
+++ app/[locale]/(home)/components/RequestDemoButton.tsx
@@ -3,22 +3,27 @@ import Link from "next/dist/client/link";
 import Image from "next/image";
 import { useLocale, useTranslations } from "next-intl";
 import { cn } from "@/lib/utils";
-function RequestDemoButton() {
+
+type RequestDemoButtonProps = {
+    variant?: "default" | "blue"
+} 
+
+function RequestDemoButton({variant}:RequestDemoButtonProps) {
     const t = useTranslations("Home.hero");
     const locale = useLocale();
     const isRTL = locale === "ar";
     return (
         <Link href="/request-demo">
-                        <Button className={cn("", isRTL ? "flex-row-reverse" : "flex-row")}>
-                            <Image
-                                src="/computer.png"
-                                alt="monitor icon"
-                                width={30}
-                                height={30}
-                            />
-                            <span className="capitalize">{t("cta")}</span>
-                        </Button>
-                    </Link>
+            <Button className={cn("", isRTL ? "flex-row-reverse" : "flex-row",variant ==="blue" ? "bg-[#115687]! hover:bg-[#115687]/80! transition-colors duration-200":"") }>
+                <Image
+                    src="/computer.png"
+                    alt="monitor icon"
+                    width={30}
+                    height={30}
+                 />
+                <span className="capitalize">{t("cta")}</span>
+             </Button>
+        </Link>
     );
 }
 
--- app/[locale]/(home)/sections/DimaAiSection.tsx
+++ app/[locale]/(home)/sections/DimaAiSection.tsx
@@ -2,48 +2,45 @@ import { LucideMonitor } from "lucide-react";
 import SectionWrapper from "../components/SectionWrapper";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import { useTranslations } from "next-intl";
+import { useTranslations, useLocale } from "next-intl";
+import RequestDemoButton from "../components/RequestDemoButton";
 
 function DimaAiSection() {
-    const t = useTranslations("Home.dimaAi");
-    return (
-        <SectionWrapper>
-            <div
-                className="container mx-auto p-6 sm:p-12  text-white rounded-4xl bg-[#043558]">
-                <p className="text-sm font-bold mb-4">{t("badge")}</p>
+  const t = useTranslations("Home.dimaAi");
+  const locale = useLocale();
+  const isRTL = locale === "ar";
+  return (
+    <SectionWrapper>
+      <div className="container mx-auto p-6 sm:px-24  text-white rounded-4xl lg:rounded-[64px] bg-[#043558]">
+        <div className={`flex flex-col lg:flex-row justify-between items-center gap-4 ${isRTL ? 'lg:flex-row-reverse' : ''}`}>
+          {/* Text Section */}
+          <div className={`flex-1 space-y-6 text-left ${isRTL ? 'lg:text-right' : ''}`}>
+            <p className="text-sm  mb-4">{t("badge")}</p>
 
-                <div className="flex flex-col-reverse lg:flex-row justify-between items-center ">
-                    {/* Text Section */}
-                    <div className="flex-1 space-y-6 text-center lg:text-left">
-                        <h2 className="text-3xl sm:text-4xl lg:text-5xl text-[#95DDEE] leading-tight">
-                            {t("title")}
-                        </h2>
+            <h2 className="text-3xl sm:text-4xl lg:text-5xl text-[#95DDEE] leading-tight">
+              {t("title")}
+            </h2>
+            <p className="text-base sm:text-lg lg:text-xl max-w-2xl leading-relaxed mx-auto lg:mx-0">
+              {t("description")}
+            </p>
+            <RequestDemoButton variant="blue" />
+          </div>
 
-                        <p className="text-base sm:text-lg lg:text-xl font-medium max-w-2xl mx-auto lg:mx-0">
-                            {t("description")}
-                        </p>
-                    </div>
-
-                    {/* Image Section */}
-                    <figure className="relative w-full lg:flex-1 h-[220px] sm:h-[300px] lg:h-[500px]">
-                        <Image
-                            src="/dima-phone.png"
-                            alt="stats"
-                            fill
-                            className="object-contain"
-                            sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 500px"
-                            priority
-                        />
-                    </figure>
-                </div>
-
-                <Button size="2xl" className="bg-[#115687]! hover:bg-[#115687]/80! transition-colors duration-200 w-full sm:w-fit mt-4">
-                    <LucideMonitor />
-                    <span>{t("cta")}</span>
-                </Button>
-            </div>
-        </SectionWrapper>
-    );
+          {/* Image Section */}
+          <figure className={`relative w-full lg:flex-1 h-[220px] sm:h-[300px] lg:h-[500px]`}>
+            <Image
+              src="/dima-phone.png"
+              alt="stats"
+              fill
+              className="object-contain"
+              sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 500px"
+              priority
+            />
+          </figure>
+        </div>
+      </div>
+    </SectionWrapper>
+  );
 }
 
 export default DimaAiSection;
