eb9dc51b77f4092e6d9f3a76eb8c53fdf6b5b9f1
KarimChehab2003 <karimchehab2018@gmail.com>
2025-11-02 10:09:52 +0200

    - Modified request demo section - Added Testimonial section in solutions page

--- app/[locale]/solutions/[slug]/page.tsx
+++ app/[locale]/solutions/[slug]/page.tsx
@@ -1,7 +1,10 @@
+import QuestionsAnsweredSection from "../../(home)/sections/QuestionsAnsweredSection";
+import RequestDemoSection from "../../(home)/sections/RequestDemoSection";
 import ActionableInsightsSection from "../sections/ActionableInsightsSection";
 import BuiltEntreprisesSection from "../sections/BuiltEntreprisesSection";
 import HeroSection from "../sections/HeroSection";
 import StayAheadSection from "../sections/StayAheadSection";
+import TestimonialSection from "../sections/TestimonialSection";
 
 type SolutionPageProps = {
     params: Promise<{ slug: string }>
@@ -15,6 +18,9 @@ async function SolutionPage({ params }: SolutionPageProps) {
             <ActionableInsightsSection />
             <StayAheadSection />
             <BuiltEntreprisesSection />
+            <TestimonialSection />
+            <RequestDemoSection />
+            <QuestionsAnsweredSection />
         </main>
     );
 }
