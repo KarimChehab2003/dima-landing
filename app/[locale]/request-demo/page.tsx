338154d6c60b318049beedce0e9b569ae768a4aa
KarimChehab2003 <karimchehab2018@gmail.com>
2025-11-30 16:45:34 +0200

    test commit

--- app/[locale]/request-demo/page.tsx
+++ app/[locale]/request-demo/page.tsx
@@ -64,7 +64,7 @@ function RequestDemoPage() {
                         <div>
                             <RequestDemoForm className="p-0" />
                         </div>
-
+                        {/* test */}
                     </div>
 
                     {/* Image Section */}
