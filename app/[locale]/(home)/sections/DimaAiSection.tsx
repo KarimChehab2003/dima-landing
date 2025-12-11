afb845ac89cbfe0d60175593c8995bc1f312e807
karim-chehab-darwinz <kchehab@darwinz.ai>
2025-12-11 16:41:29 +0200

    - Changed to new dima phone image

--- app/[locale]/(home)/sections/DimaAiSection.tsx
+++ app/[locale]/(home)/sections/DimaAiSection.tsx
@@ -11,7 +11,7 @@ function DimaAiSection() {
     <SectionWrapper>
       <div className="container mx-auto p-6 sm:px-24  text-white rounded-4xl lg:rounded-[64px] bg-[#043558] max-w-[1348px]">
         <div
-          className={`flex flex-col lg:flex-row justify-between items-center gap-4`}
+          className={`flex flex-col lg:flex-row justify-between items-center gap-8`}
         >
           {/* Text Section */}
           <div className={`flex-1 space-y-6 text-left ${isRTL ? "text-right" : ""}`}>
@@ -27,14 +27,13 @@ function DimaAiSection() {
 
           {/* Image Section */}
           <figure
-            className={`relative w-full lg:flex-1 h-[220px] sm:h-[300px] lg:h-[500px]`}
+            className={`relative w-full lg:flex-1 h-[220px] sm:h-[300px] lg:h-[500px] max-w-[500px] mr-0 lg:-mr-6`}
           >
             <Image
-              src="https://firebasestorage.googleapis.com/v0/b/dima-landing.firebasestorage.app/o/HomePage%2Fdima%20AI.png?alt=media&token=b1d3ccaa-01c4-4900-b449-e7d0df4b7fc2"
+              src="https://firebasestorage.googleapis.com/v0/b/dima-landing.firebasestorage.app/o/HomePage%2Fdima%20AI%20.png?alt=media&token=d2c367d8-c754-4e1b-8dc3-d1c05a6473e5"
               alt="stats"
               fill
-              className="object-contain"
-              sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 500px"
+              className="object-contain "
               priority
             />
           </figure>
